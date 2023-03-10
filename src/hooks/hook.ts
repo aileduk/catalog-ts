82ce7a4f9b255a866b9ae79e88380a63104bb2d0
aileduk <kudelia1997@gmail.com>
2023-03-10 19:02:32 +0200

    fixed problems with naming

--- src/hooks/hook.ts
+++ src/hooks/hook.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
 
-import type { RootState, AppDisatch } from "../store"
+import type { RootState, AppDispatch } from "../store"
 
-export const useAppDispatch = () => useDispatch<AppDisatch>()
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
